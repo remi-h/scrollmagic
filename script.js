e3e0c739ce09813820a81a0219c0dfb3f287b16c
Remi Higuchi <remyenar@yahoo.com>
2020-05-16 23:09:53 +0900

    up

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var controller = new ScrollMagic.Controller();
 // Scene Handler
 var scene1 = new ScrollMagic.Scene({
   triggerElement: "#pinned-trigger1", // point of execution
-  duration: $(window).height() - 100, // pin element for the window height - 1
+  duration: $(window).height() - 300, // pin element for the window height - 1
   triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
   reverse: true // allows the effect to trigger when scrolled in the reverse direction
 })
