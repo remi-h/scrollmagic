87204d1cbfec4e86f360dc50664d70ce24aa3c2e
Remi Higuchi <remyenar@yahoo.com>
2020-05-20 17:42:24 +0900

    a

---
+++ demo02/demo02.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.tagline', { delay: 100 });
+ScrollReveal().reveal('.punchline', { dealy: 200 });
+ScrollReveal().reveal('.widget-list', { delay: 300});
+ScrollReveal().reveal('.widget', { interval: 500 });
+ScrollReveal().reveal('.down', { delay: 2500 });
